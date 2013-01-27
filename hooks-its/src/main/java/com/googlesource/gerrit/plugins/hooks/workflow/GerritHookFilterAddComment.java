c00667bfe2a43ef56c64ddb845bc5ab325481a7a
Christian Aistleitner <christian@quelltextlich.at>
2013-01-27 17:09:28 +0100

    Format user names in created bugzilla comments
    As AccountAttribute does not provide a String conversion method, the
    generated bugzilla comments ended in:
      ... [by com.google.gerrit.server.events.AccountAttribute@5b9b8858]
    Now they end in:
      ... [by John Doe]
    
    Change-Id: I702b83ba14ec0fd14471420e461de4d26c80baf7

--- hooks-its/src/main/java/com/googlesource/gerrit/plugins/hooks/workflow/GerritHookFilterAddComment.java
+++ hooks-its/src/main/java/com/googlesource/gerrit/plugins/hooks/workflow/GerritHookFilterAddComment.java
@@ -16,6 +16,7 @@ package com.googlesource.gerrit.plugins.hooks.workflow;
 
 import java.io.IOException;
 
+import com.google.gerrit.server.config.AnonymousCowardName;
 import com.google.gerrit.server.events.AccountAttribute;
 import com.google.gerrit.server.events.ApprovalAttribute;
 import com.google.gerrit.server.events.ChangeAbandonedEvent;
@@ -31,6 +32,9 @@ public class GerritHookFilterAddComment extends GerritHookFilter  {
 
   @Inject
   private ItsFacade its;
+
+  @Inject @AnonymousCowardName
+  private String anonymousCowardName;
 
   @Override
   public void doFilter(CommentAddedEvent hook) throws IOException {
@@ -60,8 +64,16 @@ public class GerritHookFilterAddComment extends GerritHookFilter  {
     return getChangeIdUrl(change) + " | ";
   }
 
+  private String formatAccountAttribute(AccountAttribute who) {
+    if (who != null && who.name != null) {
+      return who.name;
+    }
+    return anonymousCowardName;
+  }
+
   private String getComment(ChangeAttribute change, ChangeEvent hook, AccountAttribute who, String what) {
-    return getCommentPrefix(change) + "change " + what + " [by " + who + "]";
+    return getCommentPrefix(change) + "change " + what + " [by "
+        + formatAccountAttribute(who) + "]";
   }
 
   private String getComment(ChangeRestoredEvent hook) {
@@ -96,7 +108,7 @@ public class GerritHookFilterAddComment extends GerritHookFilter  {
     }
 
     comment.append(commentAdded.comment + " ");
-    comment.append("[by " + commentAdded.author + "]");
+    comment.append("[by " + formatAccountAttribute(commentAdded.author) + "]");
     return comment.toString();
   }
 
