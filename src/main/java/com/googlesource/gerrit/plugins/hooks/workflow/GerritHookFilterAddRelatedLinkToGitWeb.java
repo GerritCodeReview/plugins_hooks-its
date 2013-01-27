c4b7381fd7c6128f851553bc317ce62b6f7f0383
Christian Aistleitner <christian@quelltextlich.at>
2013-01-27 17:47:40 +0100

    Provide hints when logging GitWeb link generation failure
    If the configuration does not allow to generate GitWeb links (no GitWeb
    configured, canonicalWebUrl missing), a NullPointer exception was
    thrown. We now attempt to properly log the problem and hint the user to
    the required setting.
    
    Change-Id: I2a00fcbf9a38770b1eae34cbe2c266ae66d445af

--- src/main/java/com/googlesource/gerrit/plugins/hooks/workflow/GerritHookFilterAddRelatedLinkToGitWeb.java
+++ src/main/java/com/googlesource/gerrit/plugins/hooks/workflow/GerritHookFilterAddRelatedLinkToGitWeb.java
@@ -57,6 +57,12 @@ public class GerritHookFilterAddRelatedLinkToGitWeb extends GerritHookFilter {
     log.debug("Git commit " + hook.refUpdate.newRev + ": " + gitComment);
 
     URL gitUrl = getGitUrl(hook);
+    if (gitUrl == null) {
+      // gitGitUrl failed to come up with a proper URL. getGitUrl already
+      // logged the main reason for the problem, we just need to exit to avoid
+      // dereferencing gitUrl
+      return;
+    }
     String[] issues = getIssueIds(gitComment);
 
     for (String issue : issues) {
@@ -68,15 +74,30 @@ public class GerritHookFilterAddRelatedLinkToGitWeb extends GerritHookFilter {
   }
 
 
+  /**
+   * generates the URL to GitWeb for the event
+   *
+   * @return if null is returned, the configuration does not allow to come up
+   * with a GitWeb url. In that case, a message describing the problematic
+   * setting has been logged.
+   */
   private URL getGitUrl(RefUpdatedEvent hook) throws MalformedURLException,
       UnsupportedEncodingException {
     String gerritCanonicalUrl =
         gerritConfig.getString("gerrit", null, "canonicalWebUrl");
+    if (gerritCanonicalUrl == null) {
+      log.info( "No canonicalWebUrl configured. Skipping GitWeb link generation");
+      return null;
+    }
     if(!gerritCanonicalUrl.endsWith("/")) {
       gerritCanonicalUrl += "/";
     }
 
     String gitWebUrl = gitWebConfig.getUrl();
+    if (gitWebUrl == null) {
+      log.info( "No url for GitWeb found. Skipping GitWeb link generation");
+      return null;
+    }
     if (!gitWebUrl.startsWith("http")) {
       gitWebUrl = gerritCanonicalUrl + gitWebUrl;
     }
